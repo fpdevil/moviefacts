c94a48f36dadb5f002d991b02e6a0048e6e2014d
Sampath Singamsetty <sampathsingamsetty@Sampaths-MacBook-Pro.local>
2023-07-10 18:36:49 +0530

    Added route for Trending movies today

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -5,11 +5,12 @@ export const AllRoutes = () => {
     return (
         <div className="dark:bg-darkbg">
             <Routes>
-                <Route path="" element={<MoviesList apiPath="movie/now_playing" title="Home" />} />
+                <Route path="" element={<MoviesList apiPath="movie/now_playing" title="MovieFacts Home" />} />
                 <Route path="movie/:id" element={<MovieInformation />} />
-                <Route path="movies/popular" element={<MoviesList apiPath="movie/popular" title="Popular" />} />
-                <Route path="movies/top" element={<MoviesList apiPath="movie/top_rated" title="Top Rated" />} />
-                <Route path="movies/upcoming" element={<MoviesList apiPath="movie/upcoming" title="UpComing" />} />
+                <Route path="movies/popular" element={<MoviesList apiPath="movie/popular" title="Popular Movies" />} />
+                <Route path="movies/top" element={<MoviesList apiPath="movie/top_rated" title="Top Rated Movies" />} />
+                <Route path="movies/upcoming" element={<MoviesList apiPath="movie/upcoming" title="Up Coming Movies" />} />
+                <Route path="movies/trending" element={<MoviesList apiPath="trending/movie/day" title="Movies Trending Today" />} />
                 <Route path="search" element={<Search apiPath="search/movie" />} />
                 <Route path="about" element={<About />} />
                 <Route path="contact" element={<Contact />} />
