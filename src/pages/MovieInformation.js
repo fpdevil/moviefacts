e238e987f5ba7484b3206ed6c05d2d748537fdaf
Sampath Singamsetty <sampathsingamsetty@Sampaths-MacBook-Pro.local>
2023-07-27 20:44:22 +0530

    Updated the components like external movie info, genres etc to place at center along with better Rating display

--- src/pages/MovieInformation.js
+++ src/pages/MovieInformation.js
@@ -26,7 +26,7 @@ function parseDate(year) {
 
 function parseRating(rating) {
     if (rating !== undefined) {
-        return rating.toFixed(1) + '/10';
+        return rating.toFixed(1);
     } else {
         return 0;
     }
@@ -122,24 +122,39 @@ export const MovieInformation = () => {
 
 
                     {/* for the movie rating */}
-                    <div className="flex flex-col items-center text-center md:flex-row md:items-center md:text-left mt-4">
-                        <svg aria-hidden="true" className="w-7 h-7 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <title>Movie ratings</title>
-                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
-                            </path>
-                        </svg>
-                        <p className="ml-2 text-sm font-bold text-slate-800 dark:text-white">{parseRating(movie.vote_average)}</p>
-                        <span className="w-1 h-1 mx-4 bg-gray-500 rounded-full dark:bg-gray-400">
-                        </span>
-                        <span className="text-sm font-bold text-slate-800 hover:underline dark:text-slate-100">
-                            {movie.vote_count} reviews
-                        </span>
+                    {/* start Component: Basic Rating */}
+                    <div className="mt-10 flex items-center justify-center gap-x-6">
+                        <div className="hidden text-sm font-semibold sm:block -space-x-2 overflow-hidden">
+                            IMDB Rating
+                        </div>
+                        <div className="boder-none sm:border-l-2 border-slate-800 dark:border-slate-100 sm:pl-8">
+                            <div className="flex justify-center sm:justify-start place-items-baseline">
+
+
+                                <span className="flex justify-center sm:justify-start place-items-baseline text-amber-400" role="img" aria-label="star rating">
+                                    <span aria-hidden="true">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 mr-3">
+                                            <title>IMDB Movie Rating</title>
+                                            <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
+                                        </svg>
+                                    </span>
+                                </span>
+
+                                <span className="tracking-widest font-semibold text-xl text-slate-800 dark:text-white mr-0">{parseRating(movie.vote_average)}</span>
+                                <span className="text-sm">/10</span>
+                            </div>
+                            <div>
+                                <h3 className="text-md font-serif text-slate-800 dark:text-slate-100">Based on {movie.vote_count} reviews</h3>
+                            </div>
+                        </div>
                     </div>
+                    {/* end Component: Basic Rating */}
 
                     {/* enumerate through the movie genres */}
+                    {/* start Component: Genre listing */}
                     {
                         movie.genres && (
-                            <div className="my-4 flex flex-wrap gap-4 sm:justify-start justify-evenly">
+                            <div className="my-4 flex flex-wrap gap-4 justify-evenly">
                                 {movie.genres.map((genre) => (
                                     <span className="px-4 py-1 text-sm font-semibold border-2 border-slate-200 rounded-lg dark:border-slate-200 dark:text-amber-100" key={genre.id}>
                                         {genre.name}
@@ -148,10 +163,12 @@ export const MovieInformation = () => {
                             </div>
                         )
                     }
+                    {/* end Component: Genre listing */}
 
 
                     {/* for the movie information like data, time etc., */}
-                    <div className="flex flex-wrap my-4 justify-evenly gap-4 sm:justify-start">
+                    {/* start Component: movie information */}
+                    <div className="flex my-4 flex-wrap justify-evenly gap-4 px-2 justify-items-center">
                         {movie.releases && (
                             <div className="flex-1">
                                 <p className="mb-1 text-slate-400">
@@ -195,40 +212,53 @@ export const MovieInformation = () => {
                             </p>
                         </div>
                     </div>
+                    {/* end Component: movie information */}
 
                     {/* Movie Overview information */}
+                    {/* start Component: movie overview */}
                     <p className="mt-4 mb-1 text-slate-600 font-bold text-center lg:text-left dark:text-slate-400">
                         Overview
                     </p>
                     <p className="text-base text-justify indent-8 my-4">
                         {movie.overview}
                     </p>
+                    {/* end Component: movie overview */}
 
-                    {/* Movie financials */}
-                    <div className="flex flex-wrap items-center font-bold max-w-2xl my-4 content-start sm:content-end md:content-center lg:content-between xl:content-around">
-                        {/* for the movie budget information */}
-                        <div className="flex-1">
-                            <p className="mb-1 text-slate-600 dark:text-slate-400">
-                                Budget
-                            </p>
-                            <p className="text-sm font-medium">
-                                {parseCurrency(movie.budget)}
-                            </p>
-                        </div>
+                    {/* Movie budget and finance */}
+                    {/* start Component: movie budget and external movie links */}
+                    <div className="w-full">
+                        <div className="grid w-full grid-cols-2 gap-0">
+                            <div className="m-3 mx-auto flex w-32 p-4">
+                                {/* for the movie budget information */}
+                                <div className="flex-auto">
+                                    <p className="mb-1 text-slate-600 dark:text-slate-400">
+                                        Budget
+                                    </p>
+                                    <p className="text-sm font-medium">
+                                        {parseCurrency(movie.budget)}
+                                    </p>
+                                </div>
+                            </div>
 
-                        {/* for the movie revenue information */}
-                        <div className="flex-1">
-                            <p className="mb-1 text-slate-600 dark:text-slate-400">
-                                Revenue
-                            </p>
-                            <p className="text-sm font-medium">
-                                {parseCurrency(movie.revenue)}
-                            </p>
+                            {/* for the movie revenue information */}
+                            <div className="m-3 mx-auto flex w-32 p-4">
+                                <div className="flex-auto">
+                                    <p className="mb-1 text-slate-600 dark:text-slate-400">
+                                        Revenue
+                                    </p>
+                                    <p className="text-sm font-medium">
+                                        {parseCurrency(movie.revenue)}
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
+                    {/* end Component: movie budget and external movie links */}
 
                     {/* for the movie IMDB ID information */}
+                    {/* start Component: external movie links */}
                     <ExternalMovieInfo imdb={movie.imdb_id} website={movie.homepage} />
+                    {/* end Component: external movie links */}
                 </div>
             </section>
         </main>
